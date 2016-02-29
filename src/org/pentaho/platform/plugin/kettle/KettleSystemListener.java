7de87ccec986ae341417318115ca7ad099b4f6e1
Rob Fellows <rfellows@pentaho.com>
2016-02-29 15:56:32 -0500

    [CLEANUP] - fixing checkstyle issues

--- src/org/pentaho/platform/plugin/kettle/KettleSystemListener.java
+++ src/org/pentaho/platform/plugin/kettle/KettleSystemListener.java
@@ -28,7 +28,6 @@ import java.util.Properties;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.pentaho.di.cluster.SlaveServer;
-import org.pentaho.di.core.Const;
 import org.pentaho.di.core.KettleEnvironment;
 import org.pentaho.di.core.Props;
 import org.pentaho.di.core.exception.KettleException;
@@ -49,109 +48,114 @@ import org.w3c.dom.Node;
 public class KettleSystemListener implements IPentahoSystemListener {
 
   /**
-   * If {@code true}, send Kettle output to the platform log file (e.g. pentaho.log) in addition to its normal 
+   * If {@code true}, send Kettle output to the platform log file (e.g. pentaho.log) in addition to its normal
    * destinations.
    */
   private boolean usePlatformLogFile = true;
-  
-  public boolean startup(final IPentahoSession session) {
-    
-    if (usePlatformLogFile) {
+
+  public boolean startup( final IPentahoSession session ) {
+
+    if ( usePlatformLogFile ) {
       initLogging();
     }
-    
+
     hookInDataSourceProvider();
-    
+
     try {
-      KettleSystemListener.environmentInit(session);
-    } catch (Throwable t) {
+      KettleSystemListener.environmentInit( session );
+    } catch ( Throwable t ) {
       t.printStackTrace();
-      Logger.error(KettleSystemListener.class.getName(), Messages.getInstance()
-          .getErrorString("KettleSystemListener.ERROR_0001_PLUGIN_LOAD_FAILED")); //$NON-NLS-1$
+      Logger.error( KettleSystemListener.class.getName(), Messages.getInstance()
+        .getErrorString( "KettleSystemListener.ERROR_0001_PLUGIN_LOAD_FAILED" ) ); //$NON-NLS-1$
     }
-    
+
     try {
-      String slaveServerConfigFilename = "system" + File.separator + "kettle" + File.separator + "slave-server-config.xml"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-      File slaveServerConfigFile = new File(PentahoSystem.getApplicationContext().getSolutionPath(slaveServerConfigFilename));
-      if (slaveServerConfigFile.exists()) {
-        InputStream is = new FileInputStream(slaveServerConfigFile);
-        Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(is);
-        Node configNode = XMLHandler.getSubNode(document, SlaveServerConfig.XML_TAG);
-        SlaveServerConfig config = new SlaveServerConfig(new LogChannel("Slave server config"), configNode);
-        config.setFilename(slaveServerConfigFilename);
+      String slaveServerConfigFilename = "system" + File.separator + "kettle" + File.separator
+        + "slave-server-config.xml"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+      File slaveServerConfigFile =
+        new File( PentahoSystem.getApplicationContext().getSolutionPath( slaveServerConfigFilename ) );
+      if ( slaveServerConfigFile.exists() ) {
+        InputStream is = new FileInputStream( slaveServerConfigFile );
+        Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse( is );
+        Node configNode = XMLHandler.getSubNode( document, SlaveServerConfig.XML_TAG );
+        SlaveServerConfig config = new SlaveServerConfig( new LogChannel( "Slave server config" ), configNode );
+        config.setFilename( slaveServerConfigFilename );
         SlaveServer slaveServer = new SlaveServer();
-        config.setSlaveServer(slaveServer);
-        CarteSingleton.setSlaveServerConfig(config);
+        config.setSlaveServer( slaveServer );
+        CarteSingleton.setSlaveServerConfig( config );
       }
-    } catch (Throwable t) {
+    } catch ( Throwable t ) {
       t.printStackTrace();
-      Logger.error(KettleSystemListener.class.getName(), t.getMessage());
+      Logger.error( KettleSystemListener.class.getName(), t.getMessage() );
     }
-    
+
     return true;
   }
 
   /**
    * Sends Kettle's logs to the platform log as well.
    */
-  @SuppressWarnings("unchecked")
+  @SuppressWarnings( "unchecked" )
   protected void initLogging() {
     // get platform's file appender
     Enumeration<org.apache.log4j.Appender> appenders = org.apache.log4j.Logger.getRootLogger().getAllAppenders();
-    while (appenders.hasMoreElements()) {
+    while ( appenders.hasMoreElements() ) {
       org.apache.log4j.Appender appender = appenders.nextElement();
-      if (appender instanceof org.apache.log4j.FileAppender) {
+      if ( appender instanceof org.apache.log4j.FileAppender ) {
         // make sure kettle's output goes to platform's file appender too
-        LogWriter.getInstance().addAppender(appender);
+        LogWriter.getInstance().addAppender( appender );
       }
     }
   }
-  
+
   private void hookInDataSourceProvider() {
     try {
-      @SuppressWarnings("unused")
-      Class clazz = Class.forName("org.pentaho.di.core.database.DataSourceProviderInterface"); //$NON-NLS-1$
+      @SuppressWarnings( "unused" )
+      Class clazz = Class.forName( "org.pentaho.di.core.database.DataSourceProviderInterface" ); //$NON-NLS-1$
       PlatformKettleDataSourceProvider.hookupProvider();
-    } catch (Exception ignored) {
+    } catch ( Exception ignored ) {
       // if here, then it's because we're running with an older
       // kettle.
     }
   }
-  
-  public static Map readProperties(final IPentahoSession session) {
+
+  public static Map readProperties( final IPentahoSession session ) {
 
     Properties props = new Properties();
-    String kettlePropsFilename = "system" + File.separator + "kettle" + File.separator + "kettle.properties"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    String kettlePropsFilename = "system" + File.separator + "kettle" + File.separator
+      + "kettle.properties"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
     InputStream is = null;
     try {
-      File kettlePropsFile = new File(PentahoSystem.getApplicationContext().getSolutionPath(kettlePropsFilename));
-      if (!kettlePropsFile.exists()) {
+      File kettlePropsFile = new File( PentahoSystem.getApplicationContext().getSolutionPath( kettlePropsFilename ) );
+      if ( !kettlePropsFile.exists() ) {
         return props;
       }
-      is = ActionSequenceResource.getInputStream(kettlePropsFilename, null);
-      props.load(is);
-    } catch (IOException ioe) {
-      Logger.error(KettleSystemListener.class.getName(), Messages.getInstance()
-          .getString("KettleSystemListener.ERROR_0003_PROPERTY_FILE_READ_FAILED") + ioe.getMessage(), ioe); //$NON-NLS-1$
+      is = ActionSequenceResource.getInputStream( kettlePropsFilename, null );
+      props.load( is );
+    } catch ( IOException ioe ) {
+      Logger.error( KettleSystemListener.class.getName(), Messages.getInstance()
+          .getString( "KettleSystemListener.ERROR_0003_PROPERTY_FILE_READ_FAILED" ) + ioe.getMessage(),
+        ioe ); //$NON-NLS-1$
     } finally {
-      if (is != null) {
+      if ( is != null ) {
         try {
           is.close();
-        } catch (IOException e) {
+        } catch ( IOException e ) {
           // ignore
         }
       }
     }
 
-    props.put("pentaho.solutionpath", PentahoSystem.getApplicationContext().getFileOutputPath("")); //$NON-NLS-1$ //$NON-NLS-2$
+    props.put( "pentaho.solutionpath",
+      PentahoSystem.getApplicationContext().getFileOutputPath( "" ) ); //$NON-NLS-1$ //$NON-NLS-2$
     return props;
 
   }
 
-  public static void environmentInit(final IPentahoSession session) throws KettleException {
+  public static void environmentInit( final IPentahoSession session ) throws KettleException {
     // init kettle without simplejndi
-    KettleEnvironment.init(false);
+    KettleEnvironment.init( false );
     Props.init( null );
   }
 
@@ -159,7 +163,7 @@ public class KettleSystemListener implements IPentahoSystemListener {
     // Nothing required
   }
 
-  public void setUsePlatformLogFile(final boolean usePlatformLogFile) {
+  public void setUsePlatformLogFile( final boolean usePlatformLogFile ) {
     this.usePlatformLogFile = usePlatformLogFile;
   }
 
