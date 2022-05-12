5fd0b1cb5d3fa4f360f8a19c9adc61b709458ae6
Peter Rinehart <peter.rinehart@hitachivantara.com>
2022-05-12 16:33:09 -0400

    [SP-6138] [TEST] Unit test fix; broken by a change in pentaho-platform.

--- core/src/test/java/org/pentaho/platform/plugin/kettle/PlatformKettleDataSourceProviderTest.java
+++ core/src/test/java/org/pentaho/platform/plugin/kettle/PlatformKettleDataSourceProviderTest.java
@@ -12,7 +12,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2020 Hitachi Vantara..  All rights reserved.
+ * Copyright (c) 2021-2022 Hitachi Vantara..  All rights reserved.
  */
 
 
@@ -20,40 +20,47 @@ package org.pentaho.platform.plugin.kettle;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.pentaho.database.service.IDatabaseDialectService;
 import org.pentaho.di.core.database.DataSourceProviderInterface;
 import org.pentaho.platform.api.data.IPooledDatasourceService;
-import org.pentaho.platform.plugin.action.kettle.PlatformKettleDataSourceProvider;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.pentaho.platform.api.engine.IPentahoSession;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
 
 import javax.sql.DataSource;
 import java.util.UUID;
 
-import static org.powermock.api.mockito.PowerMockito.doReturn;
-import static org.powermock.api.mockito.PowerMockito.mock;
-import static org.powermock.api.mockito.PowerMockito.when;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.nullable;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
-@RunWith( PowerMockRunner.class )
-@PrepareForTest( org.pentaho.platform.plugin.action.kettle.PlatformKettleDataSourceProvider.class )
+@RunWith( MockitoJUnitRunner.class )
 public class PlatformKettleDataSourceProviderTest {
 
   @Test
   public void testInvalidateNamedDataSource() throws Exception {
-    DataSourceProviderInterface dsp = mock( PlatformKettleDataSourceProvider.class );
+    try ( MockedStatic<PentahoSystem> pentahoSystemMockedStatic = Mockito.mockStatic( PentahoSystem.class ) ) {
+      IPooledDatasourceService service = mock( IPooledDatasourceService.class );
+      IDatabaseDialectService mockDatabaseDialectService = mock( IDatabaseDialectService.class );
+      pentahoSystemMockedStatic.when(
+        () -> PentahoSystem.get( eq( IPooledDatasourceService.class ), nullable( IPentahoSession.class ) ) ).thenReturn( service );
+      pentahoSystemMockedStatic.when( () -> PentahoSystem.get( eq( IDatabaseDialectService.class ) ) ).thenReturn( mockDatabaseDialectService );
+      DataSourceProviderInterface dsp = mock( PlatformKettleDataSourceProvider.class );
 
-    String namedDataSource = UUID.randomUUID().toString();
+      String namedDataSource = UUID.randomUUID().toString();
 
-    // Mock objects
-    IPooledDatasourceService service = mock( IPooledDatasourceService.class );
-    DataSource dataSource = mock( DataSource.class );
-    doReturn( service ).when( dsp, "getService", IPooledDatasourceService.class );
-    when( service.getDataSource( namedDataSource ) ).thenReturn( dataSource );
-    when( dsp.invalidateNamedDataSource( namedDataSource, DataSourceProviderInterface.DatasourceType.POOLED ) )
-      .thenCallRealMethod();
+      // Mock objects
+      DataSource dataSource = mock( DataSource.class );
+      when( service.getDataSource( namedDataSource ) ).thenReturn( dataSource );
+      when( dsp.invalidateNamedDataSource( namedDataSource, DataSourceProviderInterface.DatasourceType.POOLED ) )
+        .thenCallRealMethod();
 
-    dsp.invalidateNamedDataSource( namedDataSource, DataSourceProviderInterface.DatasourceType.POOLED );
-    Mockito.verify( service, Mockito.times( 1 ) ).clearDataSource( namedDataSource );
+      dsp.invalidateNamedDataSource( namedDataSource, DataSourceProviderInterface.DatasourceType.POOLED );
+      Mockito.verify( service, Mockito.times( 1 ) ).clearDataSource( namedDataSource );
+    }
   }
 }
