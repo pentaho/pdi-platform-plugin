d0c4336b95a3a0937d78e6ec6c5e0af42d21db3c
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-24 16:26:58 -0400

    HNC-766: updated license and copyright headers

--- core/src/main/java/org/pentaho/platform/plugin/kettle/ParametersBean.java
+++ core/src/main/java/org/pentaho/platform/plugin/kettle/ParametersBean.java
@@ -1,3 +1,14 @@
+/*! ******************************************************************************
+ *
+ * Pentaho
+ *
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
+ *
+ * Change Date: 2028-08-13
+ ******************************************************************************/
 /**
  * The Hitachi Vantara proprietary code is licensed under the terms and conditions
  * of the software license agreement entered into between the entity licensing
