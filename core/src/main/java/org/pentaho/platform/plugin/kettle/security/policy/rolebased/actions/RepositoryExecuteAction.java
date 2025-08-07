5f0048042ddb0c23d3532875d3c661c16b656d4b
Duarte Cunha Leão <duarte.leao@gmail.com>
2025-08-07 18:19:58 +0100

    feat(authorization): contract changes to IAuthorizationAction [PPUC-122]

--- core/src/main/java/org/pentaho/platform/plugin/kettle/security/policy/rolebased/actions/RepositoryExecuteAction.java
+++ core/src/main/java/org/pentaho/platform/plugin/kettle/security/policy/rolebased/actions/RepositoryExecuteAction.java
@@ -12,24 +12,29 @@
 
 package org.pentaho.platform.plugin.kettle.security.policy.rolebased.actions;
 
+import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
 import org.pentaho.platform.plugin.kettle.messages.Messages;
-import org.pentaho.platform.security.policy.rolebased.actions.AbstractAuthorizationAction;
+import org.pentaho.platform.security.policy.rolebased.actions.AbstractLocalizedAuthorizationAction;
 
-public class RepositoryExecuteAction extends AbstractAuthorizationAction {
+public class RepositoryExecuteAction extends AbstractLocalizedAuthorizationAction {
   public static final String NAME = "org.pentaho.repository.execute";
 
+  @NonNull
   @Override
   public String getName() {
     return NAME;
   }
 
+  @NonNull
   @Override
-  public String getLocalizedDisplayName( String localeString ) {
+  public String getLocalizedDisplayName( @Nullable String localeString ) {
     return Messages.getInstance().getString( NAME );
   }
 
+  @Nullable
   @Override
-  public String getLocalizedDescription( String localeString ) {
+  public String getLocalizedDescription( @Nullable String localeString ) {
     return Messages.getInstance().getString( NAME + ".description" );
   }
 }
